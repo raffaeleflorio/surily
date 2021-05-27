ad49ef7f0358b7de1eef5fa803e00771954d0404
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-05-27 10:32:25 +0200

    EnglishUpperCaseAlphabet(Test)

---
+++ src/test/java/io/github/raffaeleflorio/surily/EnglishUpperCaseAlphabetTest.java
@@ -0,0 +1,41 @@
+/*
+   Copyright 2021 Raffaele Florio
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package io.github.raffaeleflorio.surily;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+
+class EnglishUpperCaseAlphabetTest {
+  @Test
+  void testIterator() throws Throwable {
+    assertIterableEquals(
+      Set.of('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'),
+      new EnglishUpperCaseAlphabet()
+    );
+  }
+
+  @Test
+  void testSize() throws Throwable {
+    assertEquals(
+      26,
+      new EnglishUpperCaseAlphabet().size()
+    );
+  }
+}
