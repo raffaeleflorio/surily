72ada3e8cb6650781497a1ca51c405e5e1f9c3fd
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-05-27 12:23:54 +0200

    PercentEncoded(Test)

---
+++ src/main/java/io/github/raffaeleflorio/surily/PercentEncoded.java
@@ -0,0 +1,136 @@
+/*
+   Copyright 2021 Raffaele Florio
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package io.github.raffaeleflorio.surily;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * ASCII {@link CharSequence} with percent-encoding applied where needed
+ *
+ * @author Raffaele Florio (raffaeleflorio@protonmail.com)
+ * @see <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-2.1">RFC3986 about percent-encoding</a>
+ * @see <a href="https://en.wikipedia.org/wiki/Percent-encoding">Wikipedia about percent-encoding</a>
+ * @since 1.0.0
+ */
+public final class PercentEncoded implements CharSequence {
+  /**
+   * Builds the percent-encoded char sequence with UTF-8 to get bytes of non-ASCII characters and all RFC3986 unreserved characters
+   *
+   * @param origin The char sequence to decorate
+   * @see <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-2.3">RFC3986 unreserved characters</a>
+   * @since 1.0.0
+   */
+  public PercentEncoded(final CharSequence origin) {
+    this(origin, StandardCharsets.UTF_8);
+  }
+
+  /**
+   * Builds the percent-encoded char sequence with all RFC3986 unreserved characters
+   *
+   * @param origin  The char sequence to decorate
+   * @param charset The charset to get bytes of non-ASCII characters
+   * @see <a href="https://datatracker.ietf.org/doc/html/rfc3986#section-2.3">RFC3986 unreserved characters</a>
+   * @since 1.0.0
+   */
+  public PercentEncoded(final CharSequence origin, final Charset charset) {
+    this(
+      origin,
+      charset,
+      new MergedSet<>(
+        new MergedSet<>(
+          new MergedSet<>(
+            new EnglishLowerCaseAlphabet(),
+            new EnglishUpperCaseAlphabet()
+          ),
+          new Digits()
+        ),
+        Set.of('-', '.', '_', '~')
+      )
+    );
+  }
+
+  /**
+   * Builds the percent-encoded char sequence
+   *
+   * @param origin     The char sequence to decorate
+   * @param charset    The charset to get bytes of non-ASCII characters
+   * @param unreserved The reserved ASCII characters set
+   * @since 1.0.0
+   */
+  public PercentEncoded(final CharSequence origin, final Charset charset, final Set<Character> unreserved) {
+    this(origin, charset, unreserved, List.of('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'));
+  }
+
+  /**
+   * Builds the percent-encoded char sequence
+   *
+   * @param origin     The char sequence to decorate
+   * @param charset    The charset to get bytes of non-ASCII characters
+   * @param unreserved The unreserved characters set
+   * @param hexChars   The hexadecimal chars
+   * @since 1.0.0
+   */
+  PercentEncoded(final CharSequence origin, final Charset charset, final Set<Character> unreserved, final List<Character> hexChars) {
+    this.origin = origin;
+    this.charset = charset;
+    this.unreserved = unreserved;
+    this.hexChars = hexChars;
+  }
+
+  @Override
+  public int length() {
+    return toString().length();
+  }
+
+  @Override
+  public char charAt(final int i) {
+    return toString().charAt(i);
+  }
+
+  @Override
+  public CharSequence subSequence(final int i, final int i1) {
+    return toString().subSequence(i, i1);
+  }
+
+  @Override
+  public String toString() {
+    return origin
+      .codePoints()
+      .mapToObj(codePoint -> unreserved(codePoint) ? Character.toString(codePoint) : encoded(codePoint))
+      .collect(Collectors.joining());
+  }
+
+  private Boolean unreserved(final Integer codepoint) {
+    return codepoint < 128 && unreserved.contains(Character.toChars(codepoint)[0]);
+  }
+
+  private String encoded(final Integer codepoint) {
+    var sb = new StringBuilder();
+    for (var octect : Character.toString(codepoint).getBytes(charset)) {
+      sb.append("%").append(hexChars.get(0x0F & (octect >>> 4))).append(hexChars.get(0x0F & octect));
+    }
+    return sb.toString();
+  }
+
+  private final CharSequence origin;
+  private final Charset charset;
+  private final Set<Character> unreserved;
+  private final List<Character> hexChars;
+}
---
+++ src/test/java/io/github/raffaeleflorio/surily/PercentEncodedTest.java
@@ -0,0 +1,124 @@
+/*
+   Copyright 2021 Raffaele Florio
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package io.github.raffaeleflorio.surily;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+
+class PercentEncodedTest {
+  @Test
+  void testReservedRFC3986Characters() throws Throwable {
+    assertEquals(
+      "%3A%2F%3F%23%5B%5D%40%21%24%26%27%28%29%2A%2B%2C%3B%3D",
+      new PercentEncoded(":/?#[]@!$&'()*+,;=").toString()
+    );
+  }
+
+  @Test
+  void testUnreservedRFC3986Characters() throws Throwable {
+    var unreserved = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~";
+    assertEquals(
+      unreserved,
+      new PercentEncoded(unreserved).toString()
+    );
+  }
+
+  @Test
+  void testNonPrintableASCIICharacters() throws Throwable {
+    assertEquals(
+      "%00",
+      new PercentEncoded("\u0000").toString()
+    );
+  }
+
+  @Test
+  void testASCIIWhitespaces() throws Throwable {
+    assertEquals(
+      "%08%09%0A%0C%0D",
+      new PercentEncoded("\b\t\n\f\r").toString()
+    );
+  }
+
+  @Test
+  void testPercent() throws Throwable {
+    assertEquals(
+      "%25%25",
+      new PercentEncoded("%%").toString()
+    );
+  }
+
+  @Test
+  void testLength() throws Throwable {
+    assertEquals(
+      6,
+      new PercentEncoded("è", StandardCharsets.UTF_8).length()
+    );
+  }
+
+  @Test
+  void testSubsequence() throws Throwable {
+    assertEquals(
+      "A8",
+      new PercentEncoded("\u00e8\00e9\u00f7", StandardCharsets.UTF_8).subSequence(4, 6).toString()
+    );
+  }
+
+  @Test
+  void testCharAtWithISO_8859_1() throws Throwable {
+    assertEquals(
+      'D',
+      new PercentEncoded("\u00DC", StandardCharsets.ISO_8859_1).charAt(1)
+    );
+  }
+
+  @Test
+  void testSurrogatePair() throws Throwable {
+    assertEquals(
+      "%F0%90%B9%AD",
+      new PercentEncoded("\uD803\uDE6D").toString()
+    );
+  }
+
+  @Test
+  void testASCIIEncodingWithSurrogatePair() throws Throwable {
+    assertEquals(
+      "%3F",
+      new PercentEncoded("\uD83C\uDF09", StandardCharsets.US_ASCII).toString()
+    );
+  }
+
+  @Test
+  void testChars() throws Throwable {
+    assertIterableEquals(
+      List.of(0x25, 0x43, 0x33, 0x25, 0x42, 0x38),
+      new PercentEncoded("\u00F8", StandardCharsets.UTF_8).chars().boxed().collect(Collectors.toUnmodifiableList())
+    );
+  }
+
+  @Test
+  void testCodePoints() throws Throwable {
+    assertIterableEquals(
+      List.of(0x25, 0x43, 0x33, 0x25, 0x39, 0x37),
+      new PercentEncoded("\u00D7", StandardCharsets.UTF_8).codePoints().boxed().collect(Collectors.toUnmodifiableList())
+    );
+  }
+}
