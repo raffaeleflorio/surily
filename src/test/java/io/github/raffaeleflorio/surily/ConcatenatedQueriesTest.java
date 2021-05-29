e1d9573a9b08e21ccaa3eab95ab0e3d62b48d7a7
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-05-29 13:04:33 +0200

    ConcatenatedQueries(Test)

---
+++ src/test/java/io/github/raffaeleflorio/surily/ConcatenatedQueriesTest.java
@@ -0,0 +1,106 @@
+/*
+   Copyright 2021 Raffaele Florio
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package io.github.raffaeleflorio.surily;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConcatenatedQueriesTest {
+  @Test
+  void testEncoded() throws Throwable {
+    assertEquals(
+      "first%20query&second%20query&third%20query",
+      new ConcatenatedQueries(
+        List.of(
+          new QueryComponent.Fake("first%20query", "first query"),
+          new QueryComponent.Fake("second%20query", "second query"),
+          new QueryComponent.Fake("third%20query", "third query")
+        )
+      ).encoded(StandardCharsets.UTF_8)
+    );
+  }
+
+  @Test
+  void testAsString() throws Throwable {
+    assertEquals(
+      "first query&second query&third query",
+      new ConcatenatedQueries(
+        List.of(
+          new QueryComponent.Fake("first%20query", "first query"),
+          new QueryComponent.Fake("second%20query", "second query"),
+          new QueryComponent.Fake("third%20query", "third query")
+        )
+      ).asString()
+    );
+  }
+
+  @Test
+  void testCustomDelimiter() throws Throwable {
+    assertEquals(
+      "first%20query%3B;second%20query;third%20qu%3Bery",
+      new ConcatenatedQueries(
+        List.of(
+          new QueryComponent.Fake("first%20query;", "first query;"),
+          new QueryComponent.Fake("second%20query", "second query"),
+          new QueryComponent.Fake("third%20qu;ery", "third qu;ery")
+        ),
+        ';'
+      ).encoded(StandardCharsets.UTF_8)
+    );
+  }
+
+  @Test
+  void testDisallowedDelimiter() throws Throwable {
+    assertAll(
+      () -> assertThrowsWithMessage(
+        IllegalStateException.class,
+        () -> new ConcatenatedQueries(List.of(new QueryComponent.Fake("query", "query")), '#').encoded(StandardCharsets.UTF_8),
+        illegalDelimiter('#')
+      ),
+      () -> assertThrowsWithMessage(
+        IllegalStateException.class,
+        () -> new ConcatenatedQueries(List.of(new QueryComponent.Fake("query", "query")), '\u219D').encoded(StandardCharsets.UTF_8),
+        illegalDelimiter('\u219D')
+      ),
+      () -> assertThrowsWithMessage(
+        IllegalStateException.class,
+        () -> new ConcatenatedQueries(List.of(new QueryComponent.Fake("query", "query")), '%').encoded(StandardCharsets.US_ASCII),
+        illegalDelimiter('%')
+      ),
+      () -> assertThrowsWithMessage(
+        IllegalStateException.class,
+        () -> new ConcatenatedQueries(List.of(new QueryComponent.Fake("query", "query")), '[').encoded(StandardCharsets.ISO_8859_1),
+        illegalDelimiter('[')
+      )
+    );
+  }
+
+  private String illegalDelimiter(final Character delimiter) {
+    return String.format("Illegal delimiter: <%s>", delimiter);
+  }
+
+  private void assertThrowsWithMessage(final Class<? extends Throwable> expectedException, final Executable executable, final String expectedMessage) {
+    assertEquals(
+      expectedMessage,
+      assertThrows(expectedException, executable).getMessage()
+    );
+  }
+}
