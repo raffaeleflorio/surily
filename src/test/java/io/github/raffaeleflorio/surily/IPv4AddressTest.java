bc063c11213791156294935071613677fba9ee9e
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-05-31 21:26:49 +0200

    FIX testAsString IPv4AddressTest

--- src/test/java/io/github/raffaeleflorio/surily/IPv4AddressTest.java
+++ src/test/java/io/github/raffaeleflorio/surily/IPv4AddressTest.java
@@ -35,7 +35,7 @@ class IPv4AddressTest {
   void testAsString() throws Throwable {
     assertEquals(
       "255.255.255.0",
-      new IPv4Address(255, 255, 255, 0).encoded(StandardCharsets.US_ASCII)
+      new IPv4Address(255, 255, 255, 0).asString()
     );
   }
 
