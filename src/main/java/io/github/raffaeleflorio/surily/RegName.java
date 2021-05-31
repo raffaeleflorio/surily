d945b16c777dd7b07ec0f28d21568c76a57a7b2d
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-05-31 21:21:08 +0200

    RegName(Test)

---
+++ src/main/java/io/github/raffaeleflorio/surily/RegName.java
@@ -0,0 +1,75 @@
+/*
+   Copyright 2021 Raffaele Florio
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package io.github.raffaeleflorio.surily;
+
+import java.nio.charset.Charset;
+import java.util.function.BiFunction;
+
+/**
+ * RFC3986 compliant reg-name {@link HostSubcomponent} like DNS name
+ *
+ * @author Raffaele Florio (raffaeleflorio@protonmail.com)
+ * @since 1.0.0
+ */
+public final class RegName implements HostSubcomponent {
+  /**
+   * Builds an empty reg-name
+   *
+   * @author Raffaele Florio (raffaeleflorio@protonmail.com)
+   * @since 1.0.0
+   */
+  public RegName() {
+    this("");
+  }
+
+  /**
+   * Builds a reg-name
+   *
+   * @param regname The reg-name
+   * @since 1.0.0
+   */
+  public RegName(final CharSequence regname) {
+    this(
+      regname,
+      (s, charset) -> new PercentEncoded(s, charset, new UnionSet<>(new UnreservedCharacters(), new SubDelims()))
+    );
+  }
+
+  /**
+   * Builds a reg-name
+   *
+   * @param regname    The reg-name
+   * @param encodingFn The encoding function
+   * @since 1.0.0
+   */
+  RegName(final CharSequence regname, final BiFunction<CharSequence, Charset, CharSequence> encodingFn) {
+    this.regname = regname;
+    this.encodingFn = encodingFn;
+  }
+
+  @Override
+  public CharSequence encoded(final Charset charset) {
+    return encodingFn.apply(regname, charset).toString();
+  }
+
+  @Override
+  public String asString() {
+    return regname.toString();
+  }
+
+  private final CharSequence regname;
+  private final BiFunction<CharSequence, Charset, CharSequence> encodingFn;
+}
