04ca8ed89e1dd30cf1003e933c65e8d597933484
Raffaele Florio <raffaeleflorio@protonmail.com>
2021-06-09 18:23:00 +0200

    SchemeComponent.Fake(Test)

--- src/main/java/io/github/raffaeleflorio/surily/SchemeComponent.java
+++ src/main/java/io/github/raffaeleflorio/surily/SchemeComponent.java
@@ -15,6 +15,8 @@
  */
 package io.github.raffaeleflorio.surily;
 
+import java.nio.charset.Charset;
+
 /**
  * Scheme component of an URI
  *
@@ -23,4 +25,35 @@ package io.github.raffaeleflorio.surily;
  * @since 1.0.0
  */
 public interface SchemeComponent extends UriComponent {
+  /**
+   * {@link SchemeComponent} for testing purpose
+   *
+   * @author Raffaele Florio (raffaeleflorio@protonmail.com)
+   * @since 1.0.0
+   */
+  final class Fake implements SchemeComponent {
+    /**
+     * Builds a fake
+     *
+     * @param encoded  The encoded representation
+     * @param asString The asString representation
+     */
+    public Fake(final CharSequence encoded, final String asString) {
+      this.encoded = encoded;
+      this.asString = asString;
+    }
+
+    @Override
+    public CharSequence encoded(final Charset charset) {
+      return encoded;
+    }
+
+    @Override
+    public String asString() {
+      return asString;
+    }
+
+    private final CharSequence encoded;
+    private final String asString;
+  }
 }
